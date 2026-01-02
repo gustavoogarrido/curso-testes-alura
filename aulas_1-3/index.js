571fb96549d492bbc3c2591007d45c474fd2f0b6
gustavoogarrido <gustavoog05@hotmail.com>
2026-01-01 22:40:49 -0300

    Summarazing class 1 to 3 in a folder

---
+++ aulas_1-3/index.js
@@ -0,0 +1,11 @@
+const somaHorasExtras = (salario, valorHorasExtras) => salario + valorHorasExtras;
+
+const calculaDescontos = (salario, descontos) => salario - descontos;
+
+const multiplicaDepoisDobra = (valor1, valor2) => valor1 * valor2 * 2;
+
+export {
+  somaHorasExtras,
+  calculaDescontos,
+  multiplicaDepoisDobra,
+};
